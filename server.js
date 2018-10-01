90161b8ab833df277512a1ccdf3179589c98ef66
nisarat <nisaratnsaelim@gmail.com>
2018-10-01 09:52:20 +0700

    edit/up database

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 //หน้าwebstie
 var express = require('express');
 var pgp = require('pg-promise')();
-var db = pgp('postgres://nkwnjxuiidwrns:b72b4de42f726173c9acee8a85dd10ed1c8dc1a2ab7402a6feebbbccb8b14f85@ec2-54-163-245-44.compute-1.amazonaws.com:5432/d34ii1v5fr4h1e?ssl=true');
+var db = pgp('postgres://rzbthrbqjwrmnt:2982f8c701fb3cf462209cfed528dcc6678f92fc009079ce9e05f8d00ff61b24@ec2-54-243-147-162.compute-1.amazonaws.com:5432/d4kvflfh1d13co');
 
 var app = express();
 var bodyParser = require('body-parser');
