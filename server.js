6fa8bb98d417515037534d414bae72344442e2e2
nisarat <nisaratnsaelim@gmail.com>
2018-10-20 16:12:36 +0700

    add

--- server.js
+++ server.js
@@ -110,7 +110,6 @@ app.post('/product/update', function (req, res) {
     var title = req.body.title;
     var price = req.body.price;
     var sql = `update products set title = '${title}',price = '${price}' where id = '${id}' `;
-    //db.none
     db.any(sql)
         .then(function (data) {
             console.log('DATA:' + data);
